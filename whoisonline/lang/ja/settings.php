380ece1ea188c295c5afe137cd4a03d1a5172237
Hideaki SAWADA <chuno@live.jp>
2014-10-04 18:26:52 +0900

    Create settings.php

---
+++ whoisonline/lang/ja/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+ 
+$lang['minutesTillAway'] = 'ユーザーが Wiki から去ったとみなす待機時間（分）';
+$lang['ignoreAnonymous'] = 'チェックした場合、ログインしていないユーザーを追跡しない。';
+$lang['displayline'] = 'ポップアップユーザー一覧を作成するのに使用する HTML。置換え可能な文字列：{username}, {fullname}, {pageid}, {url}';
